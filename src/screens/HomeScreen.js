1357edfc5b3daae48749b9ad112f005055b48ce2
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-01 19:01:47 -0400

    Added loading spinner and general alert message display components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -20,9 +22,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             {loading ? (
-                <h1>Loading...</h1>
+                <Loader />
             ) : error ? (
-                <h1>{error}</h1>
+                <Message variant="danger">{error}</Message>
             ) : (
                 <Row>
                     {products.map((product) => (
