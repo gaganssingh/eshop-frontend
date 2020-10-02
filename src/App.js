dceab6e7a5d0a97d20f240b13f02325049641c7b
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-02 16:17:59 -0400

    Implemented product qty selection & add to cart functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const App = () => {
     return (
@@ -15,6 +16,8 @@ const App = () => {
                 <Container>
                     <Route path="/" exact component={HomeScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
+                    {/* "?" in :id? makes id as optional */}
+                    <Route path="/cart/:id?" component={CartScreen} />{" "}
                 </Container>
             </main>
             <Footer />
